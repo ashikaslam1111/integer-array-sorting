4ed54cc2881c4ce4ebc2a9a72ed083e30b0d6609
ashikaslam1111 <111225061+ashikaslam1111@users.noreply.github.com>
2022-11-09 09:18:43 +0600

    Add files via upload

---
+++ sorting.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int n, i,j,temp;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+//clear
+    for(i=0; i<n ; i++)
+    {
+        for(j=i+1; j<n; j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+//clear
+    for(i=0; i<n; i++)
+    {
+        printf("%d, ",arr[i]);
+    }
+
+    printf(" .....>>>> %d, ",arr[4-1]);
+    return 0;
+}
+
+
+
+
+
+
